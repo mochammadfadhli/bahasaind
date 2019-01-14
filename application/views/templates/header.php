a057de1af16664f1bdff09e197c4b5fffaf818d9
mochammad <mochammadfadhlizhil@gmail.com>
2019-01-14 21:19:53 +0700

    Creat

---
+++ application/views/templates/header.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Bahasaind</title>
+
+    <!-- Font -->
+    <link href="<?=base_url()?>assets/font/Damion-Regular.ttf" rel="stylesheet">
+
+
+    <!-- Bootstrap CSS -->
+    <link href="<?=base_url()?>assets/bootstrap-v4.1.3/css/bootstrap.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="<?=base_url()?>assets/bahasaindstyle/bahasaind.css">
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script type="text/javascript" src="<?=base_url()?>assets/jquery-3.3.1/jquery-3.3.1.js"></script>
+		<script type="text/javascript" src="<?=base_url()?>assets/bootstrap-v4.1.3/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+
+
