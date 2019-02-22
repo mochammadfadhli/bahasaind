984525fab1ae053d4494644760d4337a0fddcb83
mochammad <mochammadfadhlizhil@gmail.com>
2019-02-22 18:10:56 +0700

    Pembaruan
    - replace data sesuai dengan aplikasi
    - database baru unit lengkap 1-9
    - data kunci jawaban yang lengkap
    - query mysql untuk insert data kedalam database

--- application/views/home/cosine.php
+++ application/views/home/cosine.php
@@ -88,7 +88,7 @@
                     <div class="tab-content" id="v-pills-tabContent">
                         <div class="tab-pane fade show active" id="v-pills-unit1" role="tabpanel" aria-labelledby="v-pills-unit1-tab">
                             <img src="<?=base_url()?>assets/images/materi/1A.jpg" class="gambar-materi rounded mx-auto d-block" alt="">
-                            Nama saya Julia. Saya biasa dipanggil Juli. Usia saya tujuh tahun. Saya kelas satu sekolah dasar. Saya tinggal di Jalan Pemuda Nomor 1.
+                            Nama saya Julia. Saya biasa dipanggil Juli. Usia saya 7 tahun. Saya kelas 1 sekolah dasar. Saya tinggal di Jalan Pemuda Nomor 1. 
                             <form method="POST" id="form-jawaban-unit-1">
                                 <div class="form-group row mt-3">
                                     <label for="colFormLabelSm" class="col-sm-2 col-form-label col-form-label-sm">Jawabanmu</label>
@@ -103,7 +103,7 @@
                         </div>
                         <div class="tab-pane fade" id="v-pills-unit2" role="tabpanel" aria-labelledby="v-pills-unit2-tab">
                             <img src="<?=base_url()?>assets/images/materi/2.png" class="gambar-materi rounded mx-auto d-block" alt=""> 
-                            Perpustakaan Sekolah Dasar Tunas Muda, Nama : Alia Julia Kelas, Kelas : 5 C , Nomor Anggota : 576, Alamat Rumah : Jalan Rawamangun Muka Nomor 17 Jakarta Timur
+                            Perpustakaan Sekolah Dasar Tunas Muda, Nama : Alia Julia, Kelas : 5c, Nomor Anggota : 576, Alamat Rumah : Jalan Rawamangun Muka Nomor 17 Jakarta Timur
                             <form method="POST" id="form-jawaban-unit-2">
                                 <div class="form-group row mt-3">
                                     <label for="colFormLabelSm" class="col-sm-2 col-form-label col-form-label-sm">Jawabanmu</label>
@@ -118,7 +118,7 @@
                         </div>
                         <div class="tab-pane fade" id="v-pills-unit3" role="tabpanel" aria-labelledby="v-pills-unit3-tab">
                             <img src="<?=base_url()?>assets/images/materi/3A.png" class="gambar-materi rounded mx-auto d-block" alt="">
-                            Nama saya Ira. Saya kelas enam sekolah dasar. Badan saya kurus dan tinggi. Saya berkacamata. Rambut saya dikuncir dua. Saya berkaos biru.
+                            Nama saya Ira. Saya kelas 6 sekolah dasar. Badan saya kurus dan tinggi. Saya berkacamata. Rambut saya dikuncir dua. Saya berkaos biru. 
                             <form id="form-jawaban-unit-3">
                                 <div class="form-group row mt-3">
                                     <label for="colFormLabelSm" class="col-sm-2 col-form-label col-form-label-sm">Jawabanmu</label>
@@ -134,22 +134,21 @@
                         <div class="tab-pane fade" id="v-pills-unit4" role="tabpanel" aria-labelledby="v-pills-unit4-tab">
                             <img src="<?=base_url()?>assets/images/materi/4A.png" class="gambar-materi rounded mx-auto d-block" alt="">
                             Nama saya Mira.  Saya anak kedua.  Kakak saya bernama Rama.  Adik saya bernama Roni.  Kami tinggal bersama nenek dan kakek.  Ibu saya bernama Lana.  Dia guru. Ayah saya bernama Toni. Dia seorang arsitek.
-                            <form
-                            id="form-jawaban-unit-4">
-                            <div class="form-group row mt-3">
-                                <label for="colFormLabelSm" class="col-sm-2 col-form-label col-form-label-sm">Jawabanmu</label>
-                                <div class="col-sm-8">
-                                    <input type="text" class="form-control form-control-sm" id="input-teks-unit-4" placeholder="Masukkan Jawabanmu Disini">
+                            <form id="form-jawaban-unit-4">
+                                <div class="form-group row mt-3">
+                                    <label for="colFormLabelSm" class="col-sm-2 col-form-label col-form-label-sm">Jawabanmu</label>
+                                    <div class="col-sm-8">
+                                        <input type="text" class="form-control form-control-sm" id="input-teks-unit-4" placeholder="Masukkan Jawabanmu Disini">
+                                    </div>
+                                    <div class="col">
+                                        <button type="button" class="btn btn-secondary btn-sm" id="button-submit-unit-4"  onclick="kirimJawaban(4)">Submit</button>
+                                    </div>
                                 </div>
-                                <div class="col">
-                                    <button type="button" class="btn btn-secondary btn-sm" id="button-submit-unit-4"  onclick="kirimJawaban(4)">Submit</button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
+                            </form>
+                        </div>
                     <div class="tab-pane fade" id="v-pills-unit5" role="tabpanel" aria-labelledby="v-pills-unit5-tab">
                         <img src="<?=base_url()?>assets/images/materi/5.png" class="gambar-materi rounded mx-auto d-block" alt="">
-                        Pak  Agus  seorang  pengusaha.  Dia  bangun  tidur pukul 05.00 pagi. Setelah itu, dia mandi dan sarapan. Dia pergi ke kantor pukul 06.30 pagi. Tiba di kantor pukul 07.00. Pulang dari kantor dia senang membeli buah segar untuk di rumah. Tiba di rumah pukul 04.00 sore. Kadang-kadang dia   berjalan-jalan  bersama  anjingnya.  Pukul  19.00  dia makan  malam  bersama  keluarganya.  Biasanya,  dia menonton  berita  di  TV  pukul  20.00  malam.  Pukul  22.00 malam dia tidur.  Lala dan Lulu kakak beradik tinggal di Bandung. Setiap hari libur mereka membantu ayah dan ibu di kebun. Setiap hari mereka bangun sebelum pukul 05.00 pagi. Mereka beragama Islam.  Mereka  salat  lima  kali  sehari.  Salat  subuh  dilakukan pada pukul 05.00 pagi.  Siang hari, saat pukul 12.30 mereka salat zuhur. Sore hari, pukul 16.00 mereka salat asar. Pada saat malam mereka salat magrib (pukul 18.00) dan salat Isa (pukul 19.00). Di Indonesia, banyak orang bergama Islam. Ada pula yang beragama lain seperti Kristen, Katolik, Kong Hu Cu, Hindu, dan Budha. Mereka saling menghormati. 
+                        Pak  Agus  seorang  pengusaha.  Dia  bangun  tidur pukul 05.00 pagi. Setelah itu, dia mandi dan sarapan. Dia pergi ke kantor pukul 06.30 pagi. Tiba di kantor pukul 07.00. Pulang dari kantor dia senang membeli buah segar untuk di rumah. Tiba di rumah pukul 04.00 sore. Kadang-kadang dia   berjalan-jalan  bersama  anjingnya.  Pukul  19.00  dia makan  malam  bersama  keluarganya.  Biasanya,  dia menonton  berita  di  TV  pukul  20.00  malam.  Pukul  22.00 malam dia tidur.  
                         <form
                         id="form-jawaban-unit-1"
                         id="input-teks-unit-1"
@@ -168,7 +167,7 @@
                 </div>
                 <div class="tab-pane fade" id="v-pills-unit6" role="tabpanel" aria-labelledby="v-pills-unit6-tab">
                     <img src="<?=base_url()?>assets/images/materi/6.png" class="gambar-materi rounded mx-auto d-block" alt="">
-                    Namaku  Amelia.  Umurku  sepuluh  tahun.  Aku  sekolah  di SD Kusuma. Membaca adalah hobiku.  Sejak  kecil,  ibu  sering membacakan  dongeng untukku.  Kini,  aku  sudah  bisa membaca.  Ibu  sering mengajakku ke toko buku. Aku senang jika ibu membawaku ke toko buku. Di sana, aku bisa  membaca  buku.  Aku  sering  membaca  di perpustakaan  bersama  teman-teman.  Ayah  sudah membuatkanku rak buku. Aku bisa membaca buku setiap hari.
+                    Namaku  Amelia.  Umurku  10  tahun.  Aku  sekolah  di SD Kusuma. Membaca adalah hobiku.  Sejak  kecil,  ibu  sering membacakan  dongeng untukku.  Kini,  aku  sudah  bisa membaca.  Ibu  sering mengajakku ke toko buku. Aku senang jika ibu membawaku ke toko buku. Di sana, aku bisa  membaca  buku.  Aku  sering  membaca  di perpustakaan  bersama  teman-teman.  Ayah  sudah membuatkanku rak buku. Aku bisa membaca buku setiap hari.
                     <form
                     id="form-jawaban-unit-1"
                     id="input-teks-unit-1"
@@ -187,7 +186,7 @@
             </div>
             <div class="tab-pane fade" id="v-pills-unit7" role="tabpanel" aria-labelledby="v-pills-unit7-tab">
                 <img src="<?=base_url()?>assets/images/materi/7A.png" class="gambar-materi rounded mx-auto d-block" alt="">
-                Selamat malam bu. Maaf mengganggu untuk ruangan kelas besok ada perubahan. Kita akan belajar di ruang serba guna karena aula akan dipakai latihan. Terima kasih. Maaf mengganggu. Oke tolong disiapkan ya…. Selamat ulang tahun anakku semoga panjang umur dan sehat selalu… Untuk Ana, selamat ulang tahun Ana Mariana, semoga menjadi anak yang pintar dan berbakti pada orang tua dan berguna bagi orang di sekitarmu…. Selamat atas kelahiran putra Pertama paman. Semoga menjadi anak yang berguna bagi keluarga, bangsa, dan agama
+                Selamat malam bu. Maaf mengganggu untuk ruangan kelas besok ada perubahan. Kita akan belajar di ruang serbaguna karena aula akan dipakai latihan. Terima kasih. Maaf mengganggu. Oke tolong disiapkan ya
                 <form
                 id="form-jawaban-unit-1"
                 id="input-teks-unit-1"
@@ -206,7 +205,7 @@
         </div>
         <div class="tab-pane fade" id="v-pills-unit8" role="tabpanel" aria-labelledby="v-pills-unit8-tab">
             <img src="<?=base_url()?>assets/images/materi/8A.png" class="gambar-materi rounded mx-auto d-block" alt="">
-            Di Indonesia transportasi terbagi menjadi tiga jenis, laut, udara, dan darat. Di laut ada kapal laut dan feri. Di udara  ada  pesawat  udara  dan  helikopter.  Di  darat  ada bermacam-macam transportasi, misalnya becak, delman, transjakarta,  ojek,  bajaj,  taksi,  metromini,  angkot,  kereta api, sepeda, motor, dan bus kota. TransJakarta adalah satu dari beberapa kendaraan umum  yang  banyak  digunakan  masyarakat.  Pendingin ruangan bus dan harga yang murah membuat transportasi ini digemari banyak orang. Cukup dengan Rp3.500, -  kamu bisa pergi ke tempat yang kamu mau
+            Di Indonesia transportasi terbagi menjadi tiga jenis, laut, udara, dan darat. Di laut ada kapal laut dan kapal feri. Di udara  ada  pesawat  udara  dan  helikopter.  Di  darat  ada bermacam-macam transportasi, misalnya becak, delman, transjakarta,  ojek,  bajaj, mobil  taksi,  metromini,  angkot,  kereta api, sepeda, motor, dan bus kota. TransJakarta adalah satu dari beberapa kendaraan umum  yang  banyak  digunakan  masyarakat.  Pendingin ruangan bus dan harga yang murah membuat transportasi ini digemari banyak orang. Cukup dengan Rp3.500  kamu bisa pergi ke tempat yang kamu mau.
             <form>
                 <div class="form-group row mt-3">
                     <label for="colFormLabelSm" class="col-sm-2 col-form-label col-form-label-sm">Jawabanmu</label>
@@ -221,7 +220,7 @@
         </div>
         <div class="tab-pane fade" id="v-pills-unit9" role="tabpanel" aria-labelledby="v-pills-unit9-tab">
             <img src="<?=base_url()?>assets/images/materi/9.png" class="gambar-materi rounded mx-auto d-block" alt="">
-            Pasar Beringharjo terletak di Jalan Jenderal Ahmad Yani nomor 16,  Yogyakarta.   Banyak barang yang dapat dibeli  di  Pasar  Beringharjo,  mulai  dari batik, jajanan pasar, uang  kuno,  pakaian  anak  dan  dewasa, makanan cepat saji, bahan dasar jamu tradisional,  sembako hingga barang antik.  Pasar  Beringharjo  memiliki  berbagai  jenis batik mulai batik kain maupun sudah jadi pakaian, bahan katun hingga sutra. Koleksi batik kain dijumpai di kios pasar bagian barat sebelah utara.   Koleksi  pakaian batik  dijumpai  hampir di seluruh  pasar  bagian  barat. Selain  pakaian  batik,  kios pasar  bagian  barat  juga  menawarkan baju surjan, blangkon,  dan  sarung  tenun maupun batik. Selain  itu,   dijual  pula  sandal  dan  tas  di sekitar eskalator pasar bagian barat.  Harga batik di pasar ini  bervariasi.  Ada  yang  murah  ada  yang  mahal.  Harga satu  buah  pakaian  batik  rata-rata  Rp50.000,00  Harga masih bisa ditawar. Harga batik di pasar ini lebih murah daripada di Jakarta.
+            Pasar Beringharjo terletak di Jalan Jenderal Ahmad Yani nomor 16,  Yogyakarta.   Banyak barang yang dapat dibeli  di  Pasar  Beringharjo,  mulai  dari batik, jajanan pasar, uang  kuno,  pakaian  anak  dan  dewasa, makanan cepat saji, bahan dasar jamu tradisional,  sembako hingga barang antik.  Pasar  Beringharjo  memiliki  berbagai  jenis batik mulai batik kain maupun sudah jadi pakaian, bahan katun hingga sutra. Koleksi batik kain dijumpai di kios pasar bagian barat sebelah utara.   Koleksi  pakaian batik  dijumpai  hampir di seluruh  pasar  bagian  barat. Selain  pakaian  batik,  kios pasar  bagian  barat  juga  menawarkan baju surjan, blangkon,  dan  sarung  tenun maupun batik. Selain  itu,   dijual  pula  sandal  dan  tas  di sekitar eskalator pasar bagian barat.  Harga batik di pasar ini  bervariasi.  Ada  yang  murah  ada  yang  mahal.  Harga satu  buah  pakaian  batik  rata-rata  Rp50.000  Harga masih bisa ditawar. Harga batik di pasar ini lebih murah daripada di Jakarta.
             <form
             id="form-jawaban-unit-1"
             id="input-teks-unit-1"
--- docx/cosine_similarity.sql
+++ docx/cosine_similarity.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 04, 2019 at 11:47 AM
+-- Generation Time: Feb 22, 2019 at 12:08 PM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.2.11
 
@@ -46,17 +46,17 @@ INSERT INTO `unit_1` (`id`, `Term`, `Jumlah`) VALUES
 (5, 'dipanggil', 1),
 (6, 'juli', 1),
 (7, 'usia', 1),
-(8, 'tujuh', 1),
+(8, '7', 1),
 (9, 'tahun', 1),
 (10, 'kelas', 1),
-(11, 'satu', 1),
+(11, '1', 2),
 (12, 'sekolah', 1),
 (13, 'dasar', 1),
 (14, 'tinggal', 1),
-(15, 'dijalan', 1),
-(16, 'pemuda', 1),
-(17, 'nomor', 1),
-(18, 'satu', 1);
+(15, 'di', 1),
+(16, 'jalan', 1),
+(17, 'pemuda', 1),
+(18, 'nomor', 1);
 
 -- --------------------------------------------------------
 
@@ -84,21 +84,18 @@ INSERT INTO `unit_2` (`id`, `Term`, `Jumlah`) VALUES
 (7, 'alia', 1),
 (8, 'julia', 1),
 (9, 'kelas', 1),
-(10, 'lima', 2),
-(11, 'c', 1),
-(12, 'nomor', 2),
-(13, 'anggota', 1),
-(14, 'tujuh', 1),
-(15, 'enam', 1),
-(16, 'alamat', 1),
-(17, 'rumah', 1),
-(18, 'jalan', 1),
-(19, 'rawamangun', 1),
-(20, 'muka', 1),
-(21, 'tujuh', 1),
-(22, 'belas', 1),
-(23, 'jakarta', 1),
-(24, 'timur', 1);
+(10, '5c', 1),
+(11, 'nomor', 2),
+(12, 'anggota', 1),
+(13, '576', 1),
+(14, 'alamat', 1),
+(15, 'rumah', 1),
+(16, 'jalan', 1),
+(17, 'rawamangun', 1),
+(18, 'muka', 1),
+(19, '17', 1),
+(20, 'jakarta', 1),
+(21, 'timur', 1);
 
 -- --------------------------------------------------------
 
@@ -121,20 +118,19 @@ INSERT INTO `unit_3` (`id`, `Term`, `Jumlah`) VALUES
 (2, 'saya', 6),
 (3, 'ira', 1),
 (4, 'kelas', 1),
-(5, 'enam', 1),
+(5, '6', 1),
 (6, 'sekolah', 1),
 (7, 'dasar', 1),
 (8, 'badan', 1),
 (9, 'kurus', 1),
 (10, 'dan', 1),
 (11, 'tinggi', 1),
-(12, 'berkaca', 1),
-(13, 'mata', 1),
-(14, 'rambut', 1),
-(15, 'dikuncir', 1),
-(16, 'dua', 1),
-(17, 'berkaos', 1),
-(18, 'biru', 1);
+(12, 'berkacamata', 1),
+(13, 'rambut', 1),
+(14, 'dikuncir', 1),
+(15, 'dua', 1),
+(16, 'berkaos', 1),
+(17, 'biru', 1);
 
 -- --------------------------------------------------------
 
@@ -178,6 +174,353 @@ INSERT INTO `unit_4` (`id`, `Term`, `Jumlah`) VALUES
 (23, 'seorang', 1),
 (24, 'arsitek', 1);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `unit_5`
+--
+
+CREATE TABLE `unit_5` (
+  `id` int(11) NOT NULL,
+  `Term` text NOT NULL,
+  `Jumlah` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `unit_5`
+--
+
+INSERT INTO `unit_5` (`id`, `Term`, `Jumlah`) VALUES
+(1, 'pak', 1),
+(2, 'agus', 1),
+(3, 'seorang', 1),
+(4, 'pengusaha', 1),
+(5, 'dia', 8),
+(6, 'bangun', 1),
+(7, 'tidur', 2),
+(8, 'pukul', 7),
+(9, '05.00', 1),
+(10, 'pagi', 2),
+(11, 'setelah', 1),
+(12, 'itu', 1),
+(13, 'mandi', 1),
+(14, 'dan', 1),
+(15, 'sarapan', 1),
+(16, 'pergi', 1),
+(17, 'ke', 1),
+(18, 'kantor', 3),
+(19, '06.30', 1),
+(20, 'tiba', 2),
+(21, 'di', 4),
+(22, '07.00', 1),
+(23, 'pulang', 1),
+(24, 'dari', 1),
+(25, 'senang', 1),
+(26, 'membeli', 1),
+(27, 'buah', 1),
+(28, 'segar', 1),
+(29, 'untuk', 1),
+(30, 'rumah', 2),
+(31, '04.00', 1),
+(32, 'sore', 1),
+(33, 'kadang-kadang', 1),
+(34, 'berjalan-jalan', 1),
+(35, 'bersama', 2),
+(36, 'anjingnya', 1),
+(37, '19.00', 1),
+(38, 'makan', 1),
+(39, 'malam', 3),
+(40, 'keluarganya', 1),
+(41, 'biasanya', 1),
+(42, 'menonton', 1),
+(43, 'berita', 1),
+(44, 'TV', 1),
+(45, '20.00', 1),
+(46, '22.00', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `unit_6`
+--
+
+CREATE TABLE `unit_6` (
+  `id` int(11) NOT NULL,
+  `Term` text NOT NULL,
+  `Jumlah` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `unit_6`
+--
+
+INSERT INTO `unit_6` (`id`, `Term`, `Jumlah`) VALUES
+(1, 'namaku', 1),
+(2, 'amelia', 1),
+(3, 'umurku', 1),
+(4, '10', 1),
+(5, 'tahun', 1),
+(6, 'aku', 6),
+(7, 'sekolah', 1),
+(8, 'di', 3),
+(9, 'SD', 1),
+(10, 'kusuma', 1),
+(11, 'membaca', 5),
+(12, 'adalah', 1),
+(13, 'hobiku', 1),
+(14, 'sejak', 1),
+(15, 'kecil', 1),
+(16, 'ibu', 3),
+(17, 'sering', 3),
+(18, 'membacakan', 1),
+(19, 'dongeng', 1),
+(20, 'untukku', 1),
+(21, 'kini', 1),
+(22, 'sudah', 2),
+(23, 'bisa', 3),
+(24, 'mengajakku', 1),
+(25, 'ke', 2),
+(26, 'toko', 2),
+(27, 'buku', 5),
+(28, 'senang', 1),
+(29, 'jika', 1),
+(30, 'membawaku', 1),
+(31, 'sana', 1),
+(32, 'perpustakaan', 1),
+(33, 'bersama', 1),
+(34, 'teman-teman', 1),
+(35, 'ayah', 1),
+(36, 'membuatku', 1),
+(37, 'rak', 1),
+(38, 'setiap', 1),
+(39, 'hari', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `unit_7`
+--
+
+CREATE TABLE `unit_7` (
+  `id` int(11) NOT NULL,
+  `Term` text NOT NULL,
+  `Jumlah` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `unit_7`
+--
+
+INSERT INTO `unit_7` (`id`, `Term`, `Jumlah`) VALUES
+(1, 'selamat', 1),
+(2, 'malam', 1),
+(3, 'bu', 1),
+(4, 'maaf', 2),
+(5, 'mengganggu', 2),
+(6, 'untuk', 1),
+(7, 'ruangan', 1),
+(8, 'kelas', 1),
+(9, 'besok', 1),
+(10, 'ada', 1),
+(11, 'perubahan', 1),
+(12, 'kita', 1),
+(13, 'akan', 2),
+(14, 'belajar', 1),
+(15, 'di', 1),
+(16, 'ruang', 1),
+(17, 'serbaguna', 1),
+(18, 'karena', 1),
+(19, 'aula', 1),
+(20, 'dipakai', 1),
+(21, 'latihan', 1),
+(22, 'terima', 1),
+(23, 'kasih', 1),
+(24, 'oke', 1),
+(25, 'tolong', 1),
+(26, 'disiapkan', 1),
+(27, 'ya', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `unit_8`
+--
+
+CREATE TABLE `unit_8` (
+  `id` int(11) NOT NULL,
+  `Term` text NOT NULL,
+  `Jumlah` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `unit_8`
+--
+
+INSERT INTO `unit_8` (`id`, `Term`, `Jumlah`) VALUES
+(1, 'di', 4),
+(2, 'Indonesia', 1),
+(3, 'transportasi', 3),
+(4, 'terbagi', 1),
+(5, 'menjadi', 1),
+(6, 'tiga', 1),
+(7, 'jenis', 1),
+(8, 'laut', 3),
+(9, 'udara', 3),
+(10, 'dan', 5),
+(11, 'darat', 2),
+(12, 'ada', 3),
+(13, 'kapal', 2),
+(14, 'feri', 1),
+(15, 'pesawat', 1),
+(16, 'helikopter', 1),
+(17, 'bermacam-macam', 1),
+(18, 'misalnya', 1),
+(19, 'becak', 1),
+(20, 'delman', 1),
+(21, 'transjakarta', 2),
+(22, 'ojek', 1),
+(23, 'bajaj', 1),
+(24, 'mobil', 1),
+(25, 'taksi', 1),
+(26, 'metromini', 1),
+(27, 'angkot', 1),
+(28, 'kereta', 1),
+(29, 'api', 1),
+(30, 'sepeda', 1),
+(31, 'motor', 1),
+(32, 'bus', 2),
+(33, 'kota', 1),
+(34, 'adalah', 1),
+(35, 'satu', 1),
+(36, 'dari', 1),
+(37, 'beberapa', 1),
+(38, 'kendaraan', 1),
+(39, 'umum', 1),
+(40, 'yang', 3),
+(41, 'banyak', 2),
+(42, 'digunakan', 1),
+(43, 'masyarakat', 1),
+(44, 'pendingin', 1),
+(45, 'ruangan', 1),
+(46, 'harga', 1),
+(47, 'murah', 1),
+(48, 'membuat', 1),
+(49, 'ini', 1),
+(50, 'digemari', 1),
+(51, 'orang', 1),
+(52, 'cukup', 1),
+(53, 'dengan', 1),
+(54, 'rp3.500', 1),
+(55, 'kamu', 2),
+(56, 'bisa', 1),
+(57, 'pergi', 1),
+(58, 'ke', 1),
+(59, 'tempat', 1),
+(60, 'mau', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `unit_9`
+--
+
+CREATE TABLE `unit_9` (
+  `id` int(11) NOT NULL,
+  `Term` text NOT NULL,
+  `Jumlah` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `unit_9`
+--
+
+INSERT INTO `unit_9` (`id`, `Term`, `Jumlah`) VALUES
+(1, 'pasar', 10),
+(2, 'beringharjo', 3),
+(3, 'terletak', 1),
+(4, 'di', 8),
+(5, 'jalan', 1),
+(6, 'jenderal', 1),
+(7, 'Ahmad', 1),
+(8, 'yani', 1),
+(9, 'nomor', 1),
+(10, '16', 1),
+(11, 'Yogyakarta', 1),
+(12, 'banyak', 1),
+(13, 'barang', 2),
+(14, 'yang', 3),
+(15, 'dapat', 1),
+(16, 'dibeli', 1),
+(17, 'mulai', 2),
+(18, 'dari', 2),
+(19, 'batik', 10),
+(20, 'jajanan', 1),
+(21, 'uang', 1),
+(22, 'kuno', 1),
+(23, 'pakaian', 5),
+(24, 'anak', 1),
+(25, 'dan', 3),
+(26, 'dewasa', 1),
+(27, 'makanan', 1),
+(28, 'cepat', 1),
+(29, 'saji', 1),
+(30, 'bahan', 2),
+(31, 'dasar', 1),
+(32, 'jamu', 1),
+(33, 'tradisional', 1),
+(34, 'sembako', 1),
+(35, 'hingga', 2),
+(36, 'antik', 1),
+(37, 'memiliki', 1),
+(38, 'berbagai', 1),
+(39, 'jenis', 1),
+(40, 'kain', 2),
+(41, 'maupun', 2),
+(42, 'sudah', 1),
+(43, 'jadi', 1),
+(44, 'katun', 1),
+(45, 'sutra', 1),
+(46, 'koleksi', 2),
+(47, 'dijumpai', 2),
+(48, 'kios', 2),
+(49, 'bagian', 4),
+(50, 'barat', 4),
+(51, 'sebelah', 1),
+(52, 'utara', 1),
+(53, 'hampir', 1),
+(54, 'seluruh', 1),
+(55, 'selain', 2),
+(56, 'juga', 1),
+(57, 'menawarkan', 1),
+(58, 'baju', 1),
+(59, 'surjan', 1),
+(60, 'blangkon', 1),
+(61, 'sarung', 1),
+(62, 'itu', 1),
+(63, 'dijual', 1),
+(64, 'pula', 1),
+(65, 'sandal', 1),
+(66, 'tas', 1),
+(67, 'sekitar', 1),
+(68, 'eskalator', 1),
+(69, 'harga', 4),
+(70, 'ini', 2),
+(71, 'bervariasi', 1),
+(72, 'ada', 2),
+(73, 'murah', 2),
+(74, 'mahal', 1),
+(75, 'satu', 1),
+(76, 'buah', 1),
+(77, 'rata-rata', 1),
+(78, 'rp50.000', 1),
+(79, 'masih', 1),
+(80, 'bisa', 1),
+(81, 'ditawar', 1),
+(82, 'lebih', 1),
+(83, 'tenun', 1),
+(84, 'daripada', 1),
+(85, 'Jakarta', 1);
+
 --
 -- Indexes for dumped tables
 --
@@ -207,6 +550,36 @@ ALTER TABLE `unit_4`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `unit_5`
+--
+ALTER TABLE `unit_5`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `unit_6`
+--
+ALTER TABLE `unit_6`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `unit_7`
+--
+ALTER TABLE `unit_7`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `unit_8`
+--
+ALTER TABLE `unit_8`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `unit_9`
+--
+ALTER TABLE `unit_9`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -220,19 +593,49 @@ ALTER TABLE `unit_1`
 -- AUTO_INCREMENT for table `unit_2`
 --
 ALTER TABLE `unit_2`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `unit_3`
 --
 ALTER TABLE `unit_3`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `unit_4`
 --
 ALTER TABLE `unit_4`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT for table `unit_5`
+--
+ALTER TABLE `unit_5`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+
+--
+-- AUTO_INCREMENT for table `unit_6`
+--
+ALTER TABLE `unit_6`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+
+--
+-- AUTO_INCREMENT for table `unit_7`
+--
+ALTER TABLE `unit_7`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT for table `unit_8`
+--
+ALTER TABLE `unit_8`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
+
+--
+-- AUTO_INCREMENT for table `unit_9`
+--
+ALTER TABLE `unit_9`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=86;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
